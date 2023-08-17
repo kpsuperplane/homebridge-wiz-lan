333cd75e8d4115a71e0d3ffc8420ac611936bfd1
Tobias Jeppesen <github@tobiasjeppesen.com>
2023-08-17 08:51:17 +0200

    Ignore devices (#137)
    * Ignore devices from config
    
    * Ignore already added accessoraries
    
    * Debug log device
    
    * Unregister device in Apple Home when ignored
    
    * Adding comment for unregisterPlatformAccessories

--- src/wiz.ts
+++ src/wiz.ts
@@ -1,21 +1,20 @@
 import { Socket } from "dgram";
 import {
   API,
+  Characteristic,
   Logger,
   PlatformAccessory,
   Service,
-  Characteristic,
 } from "homebridge";
 
+import Accessories, { WizAccessory } from './accessories';
 import { PLATFORM_NAME, PLUGIN_NAME } from "./constants";
 import { Config, Device } from "./types";
-import Accessories, { WizAccessory } from './accessories';
 import { bindSocket, createSocket, registerDiscoveryHandler, sendDiscoveryBroadcast } from "./util/network";
 
 export default class HomebridgeWizLan {
   public readonly Service: typeof Service = this.api.hap.Service;
-  public readonly Characteristic: typeof Characteristic = this.api.hap
-    .Characteristic;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
@@ -54,6 +53,8 @@ export default class HomebridgeWizLan {
 
     const device = platformAccessory.context as Device;
 
+    this.log.debug(`Initializing device ${device}...`)
+
     // Skip if it doesn't have the new context schema
     if (typeof device?.model !== "string") {
       return;
@@ -70,7 +71,7 @@ export default class HomebridgeWizLan {
     if (typeof AccessoryClass === 'undefined') {
       this.log.warn(`Unknown device ${device.toString()}, skipping...`);
       return;
-    } 
+    }
 
     const accessory = new AccessoryClass(platformAccessory, device, this);
     accessory.init();
@@ -99,13 +100,26 @@ export default class HomebridgeWizLan {
    * This function is invoked when homebridge restores cached accessories from disk at startup.
    * It should be used to setup event handlers for characteristics and update respective values.
    */
-  configureAccessory(accessory: PlatformAccessory) {
-    this.log.info("Loading accessory from cache:", accessory.displayName);
+  configureAccessory(platformAccessory: PlatformAccessory) {
+    this.log.info("Loading accessory from cache:", platformAccessory.displayName);
+    if (this.deviceShouldBeIgnored(platformAccessory.context as Device)) {
+      this.log.info(`Unregistering ignored device ${JSON.stringify(platformAccessory.context)}...`);
+      // This makes HB crash, BUT it removes the accessory from the cache
+      this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [platformAccessory]);
+      return;
+    }
 
-    this.initAccessory(accessory);
-
+    this.initAccessory(platformAccessory);
     // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
+    this.accessories.push(platformAccessory);
+  }
+
+  deviceShouldBeIgnored(device: Device) {
+    if (this.config.ignoredDevices?.some(ignoredDevice => ignoredDevice.mac === device.mac || ignoredDevice.host === device.ip)){
+      this.log.info(`Ignoring device with ${device.ip} and ${device.mac}...`);
+      return true;
+    }
+    return false;
   }
 
   tryAddDevice(device: Device) {
@@ -115,11 +129,17 @@ export default class HomebridgeWizLan {
     if (typeof accessory === 'undefined') {
       this.log.warn(`Unknown device ${device.model.toString()}, skipping...`);
       return;
-    } 
+    }
 
-    const uuid = this.api.hap.uuid.generate(device.mac);
+
     const defaultName = `Wiz ${accessory.getName(device)} ${device.mac}`;
     let name = defaultName
+
+    if (this.deviceShouldBeIgnored(device)) {
+      return;
+    }
+
+    const uuid = this.api.hap.uuid.generate(device.mac);
 
     const existingAccessory = this.accessories.find(
       (accessory) => accessory.UUID === uuid
@@ -146,7 +166,9 @@ export default class HomebridgeWizLan {
       // create a new accessory
       const accessory = new this.api.platformAccessory(name, uuid);
       accessory.context = device;
-
+      if (this.deviceShouldBeIgnored(device)) {
+        return;
+      }
       this.log.info("Adding new accessory:", name);
 
       this.initAccessory(accessory);
@@ -159,10 +181,13 @@ export default class HomebridgeWizLan {
       ]);
     } else {
       existingAccessory.context = device;
+      if (this.deviceShouldBeIgnored(device)) {
+        return;
+      }
       this.log.info(`Updating accessory: ${name}${name == existingAccessory.displayName ? "" : ` [formerly ${existingAccessory.displayName}]`}`);
       existingAccessory.displayName = name;
       this.api.updatePlatformAccessories([existingAccessory]);
-      // try initializing again in case it didn't the last time 
+      // try initializing again in case it didn't the last time
       // (e.g. platform upgrade)
       this.initAccessory(existingAccessory);
     }
